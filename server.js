9fb87af20d00cbf02f171dacff30ff5403cd9db3
Alyssa Jane <xcthaeh@gmail.com>
2019-06-03 16:02:45 -0400

    created get requests

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const express = require('express');
-
+const express = require("express");
 const server = express();
 
-// your code here
+const db = require("./data/accounts-model.js");
+
+server.get("/", async (req, res) => {
+  try {
+    const budgets = await db.find();
+    res.status(200).json(budgets);
+  } catch (err) {
+    res.status(500).json({ message: "Error finding budgets" });
+  }
+});
+
+server.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const budget = await db.findById(id);
+    res.status(200).json(budget);
+  } catch (err) {
+    res.status(500).json({ message: "Error finding this budget" });
+  }
+});
 
 module.exports = server;
